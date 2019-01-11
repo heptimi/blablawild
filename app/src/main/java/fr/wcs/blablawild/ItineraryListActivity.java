768fcee05ca2363ad6677c683e90ed7257acf3b4
Jérémie Salis <jeremiesalis@gmail.com>
2019-01-11 11:12:49 +0100

    "QUEST_RECYCLERVIEW".

--- app/src/main/java/fr/wcs/blablawild/ItineraryListActivity.java
+++ app/src/main/java/fr/wcs/blablawild/ItineraryListActivity.java
@@ -3,12 +3,16 @@ package fr.wcs.blablawild;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ItineraryListActivity extends AppCompatActivity {
 
@@ -32,22 +36,50 @@ public class ItineraryListActivity extends AppCompatActivity {
         this.setTitle(title);
 
 
-        ListView listTrip = findViewById(R.id.list_trip);
-        ArrayList<TripModel> results = new ArrayList<>();
+//        ListView listTrip = findViewById(R.id.list_trip);
+//        ArrayList<TripModel> results = new ArrayList<>();
+//
+//        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy-hh:mm");
+//
+//        try {
+//            results.add(new TripModel("Eric", "Cartman", sdf.parse("21/02/2017-15:30"), 15));
+//            results.add(new TripModel("Stan", "Marsh", sdf.parse("21/02/2017-16:00"), 20));
+//            results.add(new TripModel("Kenny", "Broflovski", sdf.parse("21/02/2017-16:30"), 16));
+//            results.add(new TripModel("Kyle", "McCormick", sdf.parse("21/02/2017-17:00"), 40));
+//            results.add(new TripModel("Wendy", "Testaburger", sdf.parse("21/02/2017-17:30"), 20));
+//        } catch (ParseException e) {
+//        }
+//
+//        TripAdapter adapter = new TripAdapter(this, results);
+//        listTrip.setAdapter(adapter);
+        RecyclerView listItineraries = findViewById(R.id.list_itineraries);
+// TODO : préciser le type de layout de la liste
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        listItineraries.setLayoutManager(layoutManager);
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy-hh:mm");
+        final ArrayList<ItineraryModel> itineraryModels = new ArrayList<>();
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Eric Cartman", new Date(), 15));
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Stan Marsh", new Date(), 20));
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Kenny Broflovski", new Date(), 12));
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Kyle McCormick", new Date(), 18));
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Wendy Testaburger", new Date(), 16));
 
-        try {
-            results.add(new TripModel("Eric", "Cartman", sdf.parse("21/02/2017-15:30"), 15));
-            results.add(new TripModel("Stan", "Marsh", sdf.parse("21/02/2017-16:00"), 20));
-            results.add(new TripModel("Kenny", "Broflovski", sdf.parse("21/02/2017-16:30"), 16));
-            results.add(new TripModel("Kyle", "McCormick", sdf.parse("21/02/2017-17:00"), 40));
-            results.add(new TripModel("Wendy", "Testaburger", sdf.parse("21/02/2017-17:30"), 20));
-        } catch (ParseException e) {
-        }
+// TODO : afficher la liste d'itinéraires avec un adapter
 
-        TripAdapter adapter = new TripAdapter(this, results);
-        listTrip.setAdapter(adapter);
+        final ItineraryRecyclerAdapter adapter = new ItineraryRecyclerAdapter(itineraryModels);
+        listItineraries.setAdapter(adapter);
+        listItineraries.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), listItineraries, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                ItineraryModel itinerary = itineraryModels.get(position);
+                Toast.makeText(getApplicationContext(), itinerary.getDriver() , Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
 
     }
 
